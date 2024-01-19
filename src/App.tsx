00bb1519435a292580915c9fe3e216629a2107df
jonatanjc <jcquezada.2@sudamericano.edu.ec>
2024-01-19 13:15:39 -0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(): JSX.Element {
+  const handleColorClick = (e: React.MouseEvent<HTMLDivElement>, color: string) => {
+    e.stopPropagation();
+    alert(`${color} Ecuador`);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="color-container">
+        <div className="color yellow" onClick={(e) => handleColorClick(e, 'Yellow')}>
+          <div className="color blue" onClick={(e) => handleColorClick(e, 'Blue')}>
+            <div className="color red" onClick={(e) => handleColorClick(e, 'Red')}>
+              <img
+                src="https://i.pinimg.com/originals/2f/31/97/2f3197ae2e1c65dd42a9facf0af23a06.png"
+                alt="escudo"
+                style={{ width: '40px', height: '40px' }} 
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
